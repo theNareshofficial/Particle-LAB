fff10aa94d884924c3d45c8fe86ab2e299ccf37b
theNareshofficial <thenareshoffcial@gmail.com>
2024-09-17 17:41:18 +0530

    Hello World

---
+++ OOPS/hello.java
@@ -0,0 +1,15 @@
+/*
+This file for simple Hello world output java program.
+
+*/
+
+class hello{
+            public static void main(String args[]){
+                        System.out.println("\n Hello World");
+            }
+}
+
+/*
+Output:
+Hello World
+*/
