f6d16902e7e45b7a0bbc3a4172b6e2ad8b74d118
theNareshofficial <thenareshoffcial@gmail.com>
2024-09-18 21:28:50 +0530

    DSA 1a

---
+++ DSA/1a.py
@@ -0,0 +1,14 @@
+# 1a) Sum of all items
+
+def sum_list(items):
+            sum_number = 0
+            for x in items:
+                    sum_number += x
+            return sum_number
+print("Sum is", sum_list([1,2,-8]))
+
+
+""" 
+Output : 
+Sum is -5
+"""
