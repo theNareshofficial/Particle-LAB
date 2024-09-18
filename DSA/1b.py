e0e762ae758877063a04b52ea3773f8b5f11c7bd
theNareshofficial <thenareshoffcial@gmail.com>
2024-09-18 21:29:00 +0530

    DSA 1b

---
+++ DSA/1b.py
@@ -0,0 +1,16 @@
+# 1b) Smallest number in list
+
+def smallest_num_in_list(list):
+
+            min = list[0]
+
+            for a in list:
+                    if a < min:
+                        min = a
+            return min
+print("Smallest Number", smallest_num_in_list([1, 2, -8, 0]))
+
+"""
+Output : 
+Smallest Number -8
+"""
