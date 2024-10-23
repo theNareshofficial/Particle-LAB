7b2d3875acfbea2ab8c84af742c919191439faaf
theNareshofficial <thenareshoffcial@gmail.com>
2024-10-23 09:53:54 +0530

    1(D) Insertion Sort

---
+++ OOPS/insertion_sort.java
@@ -0,0 +1,42 @@
+/*
+To write a java program for Selection sort using quadratic sorting algorithms.
+*/
+
+class InsertionSort {
+
+    void sort(int arr[]) {
+        int n = arr.length;
+        for (int i = 1; i < n; ++i) {
+            int key = arr[i];
+            int j = i - 1;
+
+            while (j >= 0 && arr[j] > key) {
+                arr[j + 1] = arr[j];
+                j = j - 1;
+            }
+            arr[j + 1] = key;
+        }
+    }
+
+    static void printArray(int arr[]) {
+        int n = arr.length;
+        for (int i = 0; i < n; ++i) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+    }
+
+    public static void main(String args[]) {
+        int arr[] = {12, 11, 13, 5, 6};
+        InsertionSort ob = new InsertionSort();
+        ob.sort(arr);
+        System.out.println("Sorted Array:");
+        printArray(arr);
+    }
+}
+
+/*
+Output:
+Sorted Array:
+5 6 11 12 13
+/*
